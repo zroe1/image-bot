f274df54026cca1d2d4bccaa199d3cc6803bbba4
zephyroe <114773939+zroe1@users.noreply.github.com>
2024-02-25 20:26:27 -0600

    update

--- better_than_pi_and_v_data.py
+++ better_than_pi_and_v_data.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 def generate_operation():
     """ Returns a random arithmetic operation symbol.
@@ -50,8 +51,6 @@ def generate_simple_linear_question(num_terms):
 def get_xs_and_ys(equation):
   equation_copy = [n for n in equation]
   colapse_multiplication_and_division(equation_copy)
-  # print("here!")
-  # print(equation_copy)
 
   xs = np.arange(0.1, 10.1, 0.1)
   ys = np.full(xs.shape, 0.0)
@@ -77,7 +76,6 @@ def colapse_multiplication_and_division(equation):
   i = 0
   while i < len(equation):
     term = equation[i]
-    # print(term)
     if term == '*' or term == '/':
       xs = np.arange(0.1, 10.1, 0.1)
 
@@ -102,40 +100,35 @@ def colapse_multiplication_and_division(equation):
       else:
         ys_temp_2 = np.full(xs.shape, float(second_term))
 
-      # print(equation[i-1:i+2])
       if operation == '*':
         equation[i] = ys_temp_1 * ys_temp_2
       else:
         equation[i] = ys_temp_1 / ys_temp_2
       equation.pop(i + 1)
       equation.pop(i - 1)
-      # print(equation)
       i -= 1
     i += 1
   return equation
 
-output_rows = 20
-output_cols = 15
 
-fig, axs = plt.subplots(output_rows, output_cols, figsize=(25, 12), dpi=50)
+output_cols = 5
+folder_path = "test"
 
-# plt.ylim([0, 50])
 
-for row in axs:
-  for ax in row:
+for i in range(400):
+    fig = plt.figure(figsize=(5, 3), dpi=30)
+    ax = fig.add_subplot(111)
+    
     ax.set_ylim([0, 50])
-    # ax.set_xlim([0, 10])
-
-    # Remove both ticks and tick labels
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_xticklabels([])
     ax.set_yticklabels([])
 
-for i in range(output_rows):
-  for j in range(output_cols):
     e = generate_equation(6)
     x, y = get_xs_and_ys(e)
-    axs[i][j].plot(x, y)
+    ax.plot(x, y)
 
-plt.show()
+    image_path = os.path.join(folder_path, f'image_{i}.png')
+    plt.savefig(image_path, bbox_inches='tight')
+    plt.close(fig)  # Close the figure to free up memory
