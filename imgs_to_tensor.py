c0ec0586aae1efd4dcf5147907f1ab40e98d640e
zephyroe <114773939+zroe1@users.noreply.github.com>
2024-03-14 19:31:05 -0500

    save imgs as tensors to file

--- imgs_to_tensor.py
+++ imgs_to_tensor.py
@@ -1,19 +1,24 @@
 import os
 from PIL import Image
 from torchvision import transforms
-import matplotlib.pyplot as plt
+import torch
 
 folder_path = "test"
 
-paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path)]
+paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith("png")]
 
 to_grey = transforms.Compose([
             transforms.Grayscale(num_output_channels=1),  # Convert to grayscale
             transforms.ToTensor(),  # Convert to tensor
 ])
 
-img = Image.open(paths[6]).convert('RGB')
-print(to_grey(img))
+all_imgs = []
 
-plt.imshow(to_grey(img).squeeze(), cmap='gray')
-plt.show()
+for path in paths:
+    img = Image.open(path).convert('RGB')
+    img = to_grey(img)
+    all_imgs.append(img)
+
+all_imgs_tensor = torch.stack(all_imgs, dim=0)
+
+torch.save(all_imgs_tensor, 'all_imgs.pt')
